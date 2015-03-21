b41642d1ac537d267134f9a4667c2359c677d381
Ilia Isupov <ilia.isupov@gmail.com>
2015-03-21 19:21:32 +0200

    Initial Commit

---
+++ ShakuroTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakuroTest
+//
+//  Created by Ilia Isupov on 21.03.15.
+//  Copyright (c) 2015 Ilia Isupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
