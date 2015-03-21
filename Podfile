aa743776257822b41fd3cec3c95a0b6113fa1537
Ilia Isupov <ilia.isupov@gmail.com>
2015-03-21 21:30:21 +0200

    Initial commit - storyboard with implemented UI, cocoapods setup, added AFNetworking to the project

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'ShakuroTest' do
+
+pod 'AFNetworking', '~> 2.5.1'
+
+end
+
+target 'ShakuroTestTests' do
+
+end
+
--- ShakuroTest/ViewController.swift
+++ ShakuroTest/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var picture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Actions
+    @IBAction func search(sender: AnyObject) {
+    }
+    
 }
 
